21cc7f4e2668001fbe73bf504fed45328fcea3fb
sharunspi <sharunkk1997@gmail.com>
2021-06-13 16:24:43 +0530

    added no of files for js,html,images

--- src/script.js
+++ src/script.js
@@ -1,17 +1,42 @@
 const {dialog} = require('electron').remote
+var result = document.getElementById('result')
 const path = require('path')
 const fs = require('fs')
 let pathFolder,newPath
 let config = {
     nodeProject:false,
-    pythonProject:false
+    pythonProject:false,
+    noOfJsFiles:0,
+    noOfHtmlFiles:0,
+    noOfImageFiles:0
 }
+Element.prototype.hide = function(){
+    this.style.display = 'none'
+}
+Element.prototype.show = function(){
+    this.style.display = ''
+}
+Element.prototype.html = function(text){
+    this.innerHTML = text
+}
+result.hide()
 var chooseBtn = document.getElementById('chooseLocation')
+let list = document.getElementsByTagName('ul')
+
 chooseBtn.addEventListener('click',function(){
         pathFolder = dialog.showOpenDialog({
         properties: ['openDirectory']  
     });
         pathFolder.then(res=>{
+            config = {
+                nodeProject:false,
+                pythonProject:false,
+                noOfJsFiles:0,
+                noOfHtmlFiles:0,
+                noOfImageFiles:0
+            }
+            list[0].html('')
+            result.show()
             newPath =res.filePaths[0]
             execution(newPath)
         })
@@ -37,8 +62,8 @@ function lookAllFiles(files){
         })
 
     }
+    
     allFiles.map(res=>{
-        let list = document.getElementsByTagName('ul')
         list[0].innerHTML += "<li> "+res+" </li> "
         
         if(res == 'node_modules'){
@@ -49,5 +74,22 @@ function lookAllFiles(files){
         if(res =='package.json'){
             console.log('found node file')
         }
+        if(res.includes('.js')){
+            config.noOfJsFiles+= 1
+        }
+        if(res.includes('.png')||res.includes('.jpeg')){
+            config.noOfImageFiles+= 1
+        }
+        if(res.includes('.html')){
+            config.noOfHtmlFiles+= 1
+        }
     })
+    let noOfJSFiles = document.getElementById('noOfJSFiles')
+    let noOfHTMLFiles = document.getElementById('noOfHTMLFiles')
+    let noOfImageFiles = document.getElementById('noOfImageFiles')
+    noOfHTMLFiles.html(config.noOfHtmlFiles)
+    noOfJSFiles.html(config.noOfJsFiles)
+    noOfImageFiles.html(config.noOfImageFiles)
 }
+
+
